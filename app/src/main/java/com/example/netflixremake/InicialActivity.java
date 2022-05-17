83a2a0d62a6120e09b7cc625f78f24095e51084a
cassiobetti <cassio_betti@yahoo.com.br>
2022-05-17 08:02:44 -0300

    App. NETFLIX REMAKE

---
+++ app/src/main/java/com/example/netflixremake/InicialActivity.java
@@ -0,0 +1,31 @@
+package com.example.netflixremake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mostrarMainActivity();
+            }
+        }, 5000);
+    }
+
+    private void mostrarMainActivity() {
+        Intent intent = new Intent(
+                InicialActivity.this,MainActivity.class
+        );
+        startActivity(intent);
+        finish();
+    }
+}
